7c60613e1b86fc54bc9e151d74582b8503530c29
Patticatti <xpatticattix@gmail.com>
2023-12-13 19:33:40 -0800

    finally works
    gyat

--- inject.js
+++ inject.js
@@ -1,6 +1,8 @@
 document.addEventListener('DOMContentLoaded', function () {
+    const rootStyles = getComputedStyle(document.documentElement);
     const flexContainer = document.getElementById('flex-container');
-  
+    const flexWidth = rootStyles.getPropertyValue('--flex-width');
+    const flexWidthInPx = (parseFloat(flexWidth) / 100) * window.innerWidth;
     function addBox(inputId) {
       const inputBox = document.getElementById(inputId);
       const message = inputBox.value.trim();
@@ -9,6 +11,10 @@ document.addEventListener('DOMContentLoaded', function () {
         const newBox = document.createElement('div');
         newBox.classList.add('box');
         newBox.textContent = message;
+        const boxWidth = newBox.getBoundingClientRect().width;
+        if (boxWidth <=flexWidthInPx){
+            newBox.style.width='min-content';
+        }
         flexContainer.appendChild(newBox);
   
         // Scroll to the bottom of the flex container
@@ -25,4 +31,6 @@ document.addEventListener('DOMContentLoaded', function () {
       }
     });
   });
+
+  
   

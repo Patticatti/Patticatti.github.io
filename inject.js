977f9ffbd152b7b1238081cf75d06e27f98dc979
Patticatti <xpatticattix@gmail.com>
2023-12-13 17:08:21 -0800

    message sys working

---
+++ inject.js
@@ -0,0 +1,34 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const flexContainer = document.getElementById('flex-container');
+  
+    function addBox(inputId) {
+      const inputBox = document.getElementById(inputId);
+      const message = inputBox.value.trim();
+  
+      if (message !== '') {
+        const newBox = document.createElement('div');
+        newBox.classList.add('box');
+        newBox.textContent = message;
+        flexContainer.appendChild(newBox);
+  
+        // Scroll to the bottom of the flex container
+        flexContainer.scrollTop = flexContainer.scrollHeight;
+  
+        // Clear the input box
+        inputBox.value = '';
+      }
+    }
+  
+    document.getElementById('topInput').addEventListener('keydown', function (event) {
+      if (event.key === 'Enter') {
+        addBox('topInput');
+      }
+    });
+  
+    document.getElementById('messageBar').addEventListener('keydown', function (event) {
+      if (event.key === 'Enter') {
+        addBox('messageBar');
+      }
+    });
+  });
+  

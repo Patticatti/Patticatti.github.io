327ac69e7cd6d6b7124a96f2424c5eabf96b06c3
Patticatti <xpatticattix@gmail.com>
2023-12-13 20:16:01 -0800

    finally workign as intended
    buh

--- inject.js
+++ inject.js
@@ -1,5 +1,6 @@
 document.addEventListener('DOMContentLoaded', function () {
-    const flexContainer = document.getElementById('right-chat-container');
+    const rightChatContainer = document.getElementById('right-chat-container');
+    const flexContainer = document.getElementById('chat-container');
     function addBox(inputId) {
       const inputBox = document.getElementById(inputId);
       const message = inputBox.value.trim();
@@ -8,7 +9,7 @@ document.addEventListener('DOMContentLoaded', function () {
         const newBox = document.createElement('div');
         newBox.classList.add('box');
         newBox.textContent = message;
-        flexContainer.appendChild(newBox);
+        rightChatContainer.appendChild(newBox);
   
         // Scroll to the bottom of the flex container
         flexContainer.scrollTop = flexContainer.scrollHeight;

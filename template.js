1c0d00b5f10756be90efda62e2438096e3e31333
Patticatti <139997703+Patticatti@users.noreply.github.com>
2023-12-22 13:35:53 -0800

    generates html from template (yes!)

--- template.js
+++ template.js
@@ -1,10 +1,6 @@
-// script.js
-
-document.addEventListener("DOMContentLoaded", function () {
-  // Create a new div element to hold your injected HTML
-  var newContent = document.createElement("div");
-  newContent.innerHTML = `
-        <header>
+var existingContent = document.getElementById("content");
+var header = document.createElement("header");
+header.innerHTML = `
         <div id="logo-container">
             <a href="index.html" id="logo">
                 <img class="logo" height="50px" width="auto" src="../images/logo.png">
@@ -19,52 +15,18 @@ document.addEventListener("DOMContentLoaded", function () {
             <a href="../contact.html">CONTACT</a>
         </nav>
         <img class="menu-icon" src="../images/burgermenu.svg" onclick="toggleNav()"></img>
-        </header>
-        
-        <div id="contact-section">
-        <div class="contact-me">
-            <div class="contact-me-startitle">
-                <img class="about-skill-title-icon" src="../images/magentastar.png">
-                <h2>CONTACT ME</h2>
-            </div>
-            <h5>For any additional questions or information, please contact me through the links below:</h5>
-            <div class="contact-social-icons">
-                <button class="social-button" onclick="window.location.href='https://www.linkedin.com/in/patricia-tani/'">
-                    <img class="social-icon" src="../images/linkedin.png">
-                </button>
-                <button class="social-button" onclick="window.location.href='https://www.instagram.com/patti_tani/'">
-                    <img class="social-icon" src="../images/instagram.png">
-                </button>
-                <button class="social-button" onclick="window.location.href='https://www.youtube.com/@itsPatticatti'">
-                    <img class="social-icon" src="../images/youtube.png">
-                </button>
-            </div>
-        </div>
-        <div class="contact-email-phone">
-            <div class="mail-number">
-                <img class="social-icon" src="../images/gmail.png">
-                <h4>xpatticattix@gmail.com</h4>
-            </div>
-            <img class="skill-line" src="../images/vertline.svg">
-            <div class="mail-number">
-                <img class="social-icon" src="../images/discord.png">
-                <h4>patticatti</h4>
-            </div>
-        </div>
-    </div>
-    <footer> 
-        <h5>© Patricia Tani 2023 - 2023 | Vancouver, British Columbia, Canada</h5>
-    </footer>
-    `;
-  document.body.appendChild(newContent);
+        `;
+document.body.insertBefore(header, document.body.firstChild);
 
+document.addEventListener("DOMContentLoaded", function () {
   var isNavOpen = false;
   var nav = document.querySelector("nav");
 
-  function toggleNav() {
+  window.toggleNav = function () {
+    console.log("togglenav");
     isNavOpen = !isNavOpen;
     nav.style.right = isNavOpen ? "0" : "-250px";
-  }
+  };
 
   document.body.addEventListener("click", function (e) {
     if (
@@ -75,4 +37,43 @@ document.addEventListener("DOMContentLoaded", function () {
       toggleNav();
     }
   });
+  var footer = document.createElement("div");
+  footer.innerHTML = `
+    <div id="contact-section">
+    <div class="contact-me">
+        <div class="contact-me-startitle">
+            <img class="about-skill-title-icon" src="../images/magentastar.png">
+            <h2>CONTACT ME</h2>
+        </div>
+        <h5>For any additional questions or information, please contact me through the links below:</h5>
+        <div class="contact-social-icons">
+            <button class="social-button" onclick="window.location.href='https://www.linkedin.com/in/patricia-tani/'">
+                <img class="social-icon" src="../images/linkedin.png">
+            </button>
+            <button class="social-button" onclick="window.location.href='https://www.instagram.com/patti_tani/'">
+                <img class="social-icon" src="../images/instagram.png">
+            </button>
+            <button class="social-button" onclick="window.location.href='https://www.youtube.com/@itsPatticatti'">
+                <img class="social-icon" src="../images/youtube.png">
+            </button>
+        </div>
+    </div>
+    <div class="contact-email-phone">
+        <div class="mail-number">
+            <img class="social-icon" src="../images/gmail.png">
+            <h4>xpatticattix@gmail.com</h4>
+        </div>
+        <img class="skill-line" src="../images/vertline.svg">
+        <div class="mail-number">
+            <img class="social-icon" src="../images/discord.png">
+            <h4>patticatti</h4>
+        </div>
+    </div>
+    </div>
+    <footer> 
+    <h5>© Patricia Tani 2023 - 2023 | Vancouver, British Columbia, Canada</h5>
+    </footer>
+    `;
+
+  document.body.appendChild(footer);
 });
